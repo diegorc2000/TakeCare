129ba7bc460f45568d0c837a68d7d6ba8bdce7f2
Diego <diegorc2000@icloud.com>
2021-02-05 08:56:28 +0100

    Añadido el java con el firebase para el singIn y el Log IN y creado otra clase para restablecer la contraseña con el Firebase.

--- app/src/main/java/com/diealbalb/MainActivity.java
+++ app/src/main/java/com/diealbalb/MainActivity.java
@@ -1,8 +1,12 @@
 package com.diealbalb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
 
         tvInicio = findViewById(R.id.tvInicio);
         tvInicio.setText(String.format(getString(R.string.tv_inicio), user.getEmail()));
+    }
+
+    //PARA EL MENU onCreateOptionsMenu y onOptionsItemSelected
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itmDesconectar){
+            fba.signOut();
+            Intent i = new Intent(this, Login.class);
+            startActivity(i);
+            finish();//para cerrar el activity
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
